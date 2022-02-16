3afdf2de97536ee5380327abecd50fb366fe747c
Gabriel Rodrigues <33073778+gabrielroodriz@users.noreply.github.com>
2022-02-16 06:46:08 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:round_six/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Round 6 Memory',
+      debugShowCheckedModeBanner: false,
+      theme: Round6Theme.theme,
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
