f6720d5e95745f5ed8ed9fad47d846a4ea68cf10
Gabriel Rodrigues <33073778+gabrielroodriz@users.noreply.github.com>
2022-02-17 14:54:30 -0300

    feat: animation card

--- lib/widgets/card_game.widget.dart
+++ lib/widgets/card_game.widget.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 import 'package:round_six/contants.dart';
 import 'package:round_six/theme.dart';
 
@@ -17,25 +19,72 @@ class CardGame extends StatefulWidget {
   _CardGameState createState() => _CardGameState();
 }
 
-class _CardGameState extends State<CardGame> {
+class _CardGameState extends State<CardGame>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animation = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+  }
+
+  @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
+  }
+
+  flipCard() {
+    if (!animation.isAnimating) {
+      animation.forward();
+      Timer(const Duration(seconds: 2), () => animation.reverse());
+    }
+  }
+
+  AssetImage getImage(double angulo) {
+    if (angulo > 0.5 * pi) {
+      return AssetImage('assets/images/${widget.opcao.toString()}.png');
+    } else {
+      return widget.modo == Modo.normal
+          ? const AssetImage('assets/images/card_normal.png')
+          : const AssetImage('assets/images/card_round.png');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          border: Border.all(
-            color:
-                widget.modo == Modo.normal ? Colors.white : Round6Theme.color,
-            width: 2,
-          ),
-          borderRadius: const BorderRadius.all(
-            Radius.circular(5.0),
-          ),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: widget.modo == Modo.normal
-                ? const AssetImage('assets/images/card_normal.png')
-                : const AssetImage('assets/images/card_round.png'),
-          )),
-    );
+    return AnimatedBuilder(
+        animation: animation,
+        builder: (context, _) {
+          final angulo = animation.value * pi;
+          final transform = Matrix4.identity()
+            ..setEntry(3, 2, 0.002)
+            ..rotateY(angulo);
+          return GestureDetector(
+            onTap: (() => flipCard()),
+            child: Transform(
+              alignment: Alignment.center,
+              transform: transform,
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: widget.modo == Modo.normal
+                          ? Colors.white
+                          : Round6Theme.color,
+                      width: 2,
+                    ),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(5.0),
+                    ),
+                    image: DecorationImage(
+                        fit: BoxFit.cover, image: getImage(angulo))),
+              ),
+            ),
+          );
+        });
   }
 }
